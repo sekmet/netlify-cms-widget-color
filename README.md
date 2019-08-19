cf58b0e803a4eb42e84fb6ba973bbe41fdeaaf68
Carlos Guimaraes <sekmet@open4g.com>
2019-08-19 17:34:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# netlify-cms-widget-color
+
+A Collection of Color Pickers from Sketch, Photoshop, Chrome, Github, Twitter, Material Design & more widget for Netlify CMS.
+
+[![npm version](https://badge.fury.io/js/netlify-cms-widget-color.svg)](https://badge.fury.io/js/netlify-cms-widget-color)
+[![GitHub version](https://badge.fury.io/gh/sekmet%2Fnetlify-cms-widget-color.svg)](https://badge.fury.io/gh/sekmet%2Fnetlify-cms-widget-color)
+[![MIT license](http://img.shields.io/badge/license-MIT-brightgreen.svg)](http://opensource.org/licenses/MIT)
+
+
+## Install
+
+As an npm package:
+
+```shell
+yarn add netlify-cms-widget-color
+
+OR
+
+npm install --save netlify-cms-widget-color
+```
+
+```js
+import NetlifyCmsWidgetColor from 'netlify-cms-widget-color';
+
+CMS.registerWidget([
+  NetlifyCmsWidgetColor.Widget()
+])
+```
+
+## How to use
+
+Add to your Netlify CMS configuration:
+
+```yaml
+    fields:
+      - { name: <fieldname>, label: <fieldlabel>, widget: fontpicker }
+```
+
+
+
+## Support
+
+For help with this widget, open an [issue](https://github.com/sekmet/netlify-cms-widget-color) or ask the Netlify CMS community in [Gitter](https://gitter.im/netlify/netlifycms).
---
+++ src/ColorControl.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import reactCSS from 'reactcss';
+import { SketchPicker } from 'react-color';
+//const uniqid = require('uniqid');
+//const pId = uniqid();
+
+export default class ColorControl extends React.Component {
+  state = {
+    displayColorPicker: false,
+    color: {
+      r: '241',
+      g: '112',
+      b: '19',
+      a: '1',
+    },
+  };
+
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    forID: PropTypes.string,
+    value: PropTypes.node,
+    classNameWrapper: PropTypes.string.isRequired,
+    setActiveStyle: PropTypes.func.isRequired,
+    setInactiveStyle: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    value: {},
+  };
+
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  };
+
+  handleClose = () => {
+    this.setState({ displayColorPicker: false })
+  };
+
+  handleChange = (color) => {
+    this.setState({ color: color.rgb })
+  };
+
+
+  render() {
+    const {
+      forID,
+      value,
+      onChange,
+      classNameWrapper,
+      setActiveStyle,
+      setInactiveStyle,
+    } = this.props;
+
+    const styles = reactCSS({
+      'default': {
+        color: {
+          width: '100%',
+          height: '58px',
+          borderRadius: '3px',
+          background: `rgba(${ this.state.color.r }, ${ this.state.color.g }, ${ this.state.color.b }, ${ this.state.color.a })`,
+        },
+        swatch: {
+          padding: '6px',
+          background: '#fff',
+          borderRadius: '1px',
+          boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+          display: 'inline-block',
+          cursor: 'pointer',
+        },
+        popover: {
+          position: 'absolute',
+          zIndex: '3',
+        },
+        cover: {
+          position: 'fixed',
+          top: '0px',
+          right: '0px',
+          bottom: '0px',
+          left: '0px',
+        },
+      },
+    });
+
+    return (
+    <div>
+      <div id={forID} className={classNameWrapper} style={ styles.swatch } onClick={ this.handleClick }>
+        <div style={ styles.color } onFocus={setActiveStyle} onBlur={setInactiveStyle} />
+      </div>
+      { this.state.displayColorPicker ? <div style={ styles.popover }>
+        <div style={ styles.cover } onClick={ this.handleClose }/>
+        <SketchPicker color={ this.state.color } value={value || this.state.color} onChange={color => {
+          this.handleChange(color)
+          return onChange(`${color.hex}`)
+        }} />
+      </div> : null }
+
+    </div>
+    );
+  }
+}
+
+{/*<input
+        type="text"
+        id={forID}
+        className={classNameWrapper}
+        value={value || ''}
+        onChange={e => onChange(e.target.value)}
+        onFocus={setActiveStyle}
+        onBlur={setInactiveStyle}
+      />*/}
